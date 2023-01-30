2cc360709a43e15c489ca0d68884925855ab939e
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-01-30 16:38:04 -0300

    Add files via upload

---
+++ notas moedas.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ int main(){
+int n;
+scanf("%d",&n);
+printf("NOTAS:\n%d nota(s) de R$ 100.00\n%d nota(s) de R$ 50.00\n%d nota(s) de R$ 20.00\n%d nota(s) de R$ 10.00\n%d nota(s) de R$ 5.00\n%d nota(s) de R$ 2.00\n", n/100,(n%100)/50,(n%50)/20,(n%50%20)/10,(n%10)/5,(n%5)/2);
+//printf("MOEDAS:\n%d moeda(s) de R$ 1.00\n%d moeda(s) de R$ 0.50\n%d moeda(s) de R$ 0.25\n%d moeda(s) de R$ 0.10\n%d moeda(s) de R$ 0.05\n%d moeda(s) de R$ 0.01",(n%5%2),(n%(1/2)));
+return 0;
+}
